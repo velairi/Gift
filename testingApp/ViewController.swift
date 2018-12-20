f61a4cdb7c8e15b86a755beb871fce590db577f3
Valerie Don <valeriedon@valeries-macbook-pro.local>
2018-12-20 15:45:53 -0800

    Initial Commit

---
+++ testingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testingApp
+//
+//  Created by Valerie Don on 12/20/18.
+//  Copyright © 2018 Valerie Don. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
